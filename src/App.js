db083b7f313ceee0035e4f85f3046509f2a95475
sebastianterleira <seb.terleira1204@gmail.com>
2023-01-05 12:52:32 -0500

    Implementing Lazy load in the application

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import AuthenticatedApp from "./AuthenticatedApp";
 import { useAuth } from "./context/auth-context";
-import UnauthenticatedApp from "./UnauthenticatedApp"
-
+import React, {Suspense, lazy} from "react";
+const AuthenticatedApp = lazy(() => import("./AuthenticatedApp"));
+const UnauthenticatedApp = lazy(() => import("./UnauthenticatedApp"));
 
 
 function App() {
   const { user } = useAuth();
   return (
     <>
+    <Suspense fallback={<h1>Cargando ... puto</h1>}>
       {user ? <AuthenticatedApp/> : <UnauthenticatedApp/>}
+    </Suspense>
     </>
   );
 }
