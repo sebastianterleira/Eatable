d38195d1e0de09528335995d344e4d9534f5d6ce
sebastianterleira <seb.terleira1204@gmail.com>
2023-01-07 17:50:08 -0500

    add: Component Product List

--- src/components/products-list.js
+++ src/components/products-list.js
@@ -1,4 +1,7 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import styled from "@emotion/styled";
+import { fonts } from "../styles/typography";
 
 const ContainerCard = styled("div")`
   display: grid;
@@ -8,18 +11,25 @@ const ContainerCard = styled("div")`
 `
 
 const CardData = styled("div")`
+display: flex;
+flex-direction: column;
+align-items: center;
 overflow: hidden;
 background-color: #FFFFFF;
 border-radius: 20px;
 margin-bottom: 45px;
 margin-top: 9px;
-box-shadow: 5px 5px 5px #48529944;
 position: relative;
+overflow: hidden;
+  transition: all 400ms ease;
+  &:hover {
+		box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+  transform: translateY(-3%);
+  }
 `;
 
 const ImagenProduct = styled("img")`
 left: 13px;
-position: absolute;
 left: 8.33%;
 right: 8.33%;
 top: 0%;
@@ -27,6 +37,29 @@ bottom: 48%;
 width: 130px;
 height: 130px;
 border-radius: 65px;
+box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+`
+
+
+const ProductName = styled.p`
+margin-left: 13px;
+margin-right: 13px;
+margin-top: 12px;
+margin-bottom: 8px;
+font-family: ${fonts.secondary};
+font-weight: 600;
+font-size: 22px;
+line-height: 27.65px;
+text-align: center;
+`
+
+const ProductPrice = styled.p`
+margin-bottom: 30px;
+font-family: ${fonts.secondary};
+font-weight: 600;
+font-size: 22px;
+line-height: 27.65px;
+color: orange;
 `
 
 function ProductList({products}) {
@@ -37,7 +70,10 @@ function ProductList({products}) {
 						products.map((product)=>(
 						<CardData key={product.id}>
 							<ImagenProduct src={product.picture_url}/>
-							<p>{product.name === null ? "No content" : product.name}</p>
+							<div css={css`display: flex;`}>
+								<ProductName>{product.name === null ? "No content" : product.name}</ProductName>
+							</div>
+							<ProductPrice>{String(product.price).split("").length === 3 ? "$" + String(product.price).split("").slice(0, -2).join("") + "," + String(product.price).split("").slice(1).join("") : String(product.price).split("").length === 2 ? "$" + String(product.price).split("").slice(0, -1).join("") + "," + String(product.price).split("").slice(1).join("") + "0" : "$" + String(product.price).split("").slice(0, -2).join("") + "," + String(product.price).split("").slice(2).join("")}</ProductPrice>
 						</CardData>
 					))}
 		</ContainerCard>
